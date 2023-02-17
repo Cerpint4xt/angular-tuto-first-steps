4a3b17063032f41a815900ed8960737bd6502a4c
Cerpint4xt <33381390+Cerpint4xt@users.noreply.github.com>
2023-02-17 16:45:32 -0400

    configuration cart service to use HttpClient

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -19,5 +20,7 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 }
