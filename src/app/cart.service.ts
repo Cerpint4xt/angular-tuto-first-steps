a7807d2683d1633187033e14e61745dbc173a118
Cerpint4xt <33381390+Cerpint4xt@users.noreply.github.com>
2023-02-17 16:53:50 -0400

    creating shipping service and managing httpclient

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -19,6 +19,10 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+  getShippingPrices() {
+    return this.http.get<{type: String, price: number}[]>
+    ('assets/shipping.json');
+  }
 
   constructor(
     private http: HttpClient
